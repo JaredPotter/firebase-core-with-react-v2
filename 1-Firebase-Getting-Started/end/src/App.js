cec5bd0ac8c0831ff7969b8603581cbb775ef424
Tyson <tysonpottersd@gmail.com>
2023-08-19 17:51:11 -0600

    Add Section 1 end

---
+++ 1-Firebase-Getting-Started/end/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import firebaseConfig from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
